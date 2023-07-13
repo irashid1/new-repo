5451b82b80c35eeaa685e3ffa886dafa2083350a
Imtiaz Rashid <hello@imtiazrashid.com>
2023-07-13 11:52:51 -0400

    completed the front-end of the task list app

--- new-app/App.js
+++ new-app/App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Keyboard, ScrollView, StyleSheet, Text, View } from "react-native";
+import TaskInputField from "./components/TaskInputField";
+import TaskItem from "./components/TaskItem";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks, task]);
+    Keyboard.dismiss();
+  };
+
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value, index) => index != deleteIndex));
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Welcome to my New App!!!</Text>
-      <Text style={styles.heading}>TODO List!</Text>
+      <Text style={styles.heading}>TODO List</Text>
       <StatusBar style="auto" />
+      <ScrollView style={styles.scrollView}>
+        {tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem
+                index={index + 1}
+                task={task}
+                deleteTask={() => deleteTask(index)}
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
+      <TaskInputField addTask={addTask} />
     </View>
   );
 }
@@ -15,15 +43,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#1E1A3C",
-    alignItems: "center",
-    justifyContent: "center",
   },
   heading: {
     color: "#fff",
-    fontSize: 15,
-    fontWeight: "200",
-    marginTop: 30,
+    fontSize: 20,
+    fontWeight: "600",
+    marginTop: 45,
     marginBottom: 10,
     marginLeft: 20,
+  },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
   },
 });
