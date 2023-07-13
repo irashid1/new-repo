0753619f457f2d3e5b6fd5d912b6e374cfd4ffe4
Imtiaz Rashid <hello@imtiazrashid.com>
2023-07-13 00:53:25 -0400

    new app

---
+++ new-app/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Welcome to my New App!!!</Text>
+      <Text style={styles.heading}>TODO List</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1E1A3C',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: '200',
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+});
