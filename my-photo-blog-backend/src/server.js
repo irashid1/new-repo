09cad4538af6e021742a51dc54d511a245448cbd
Imtiaz Rashid <hello@imtiazrashid.com>
2023-07-14 13:20:21 -0400

    added URL params in express

--- my-photo-blog-backend/src/server.js
+++ my-photo-blog-backend/src/server.js
@@ -6,7 +6,12 @@ app.use(express.json());
 
 app.post("/hello", (req, res) => {
   console.log(req.body);
-  res.send(`Hello ${req.body.name}!`);
+  res.send(`Hello ${req.body.name}`);
+});
+
+app.get("/hello/:name", (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello ${name}!!`);
 });
 
 app.listen(8000, () => {
