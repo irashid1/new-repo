4b05faca2d54abe9fb861b25f61d9fb764868cbd
Imtiaz Rashid <hello@imtiazrashid.com>
2023-07-17 00:00:36 -0400

    connected front and back end

--- my-photo-blog/src/components/CommentsList.js
+++ my-photo-blog/src/components/CommentsList.js
@@ -3,7 +3,7 @@ const CommentsList = ({ comments }) => (
     <h3>Comments:</h3>
     {comments.map((comment) => (
       <div className="comment" key={comment.postedBy + ": " + comment.text}>
-        <h4>{comment.postedBy}</h4>
+        <h4>{comment.email ? comment.email : comment.postedBy}</h4>
         <p>{comment.text}</p>
       </div>
     ))}
